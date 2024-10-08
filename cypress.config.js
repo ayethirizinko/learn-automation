760e5bf55171042795d419c39f3b088e5a870459
ayethiri.z.ko <ayethiri.z.ko@frontiir.net>
2024-10-08 10:37:24 +0630

    Add specific pattern for cypress run

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://example.cypress.io',
-    specPattern: 'cypress/e2e/*.cy.{js,jsx,ts,tsx}',
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     defaultCommandTimeout:10000,
     // execTimeout:60000,
     // taskTimeout:60000,
